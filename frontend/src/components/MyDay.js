3296376f4eb3ef76807b8606c6f350a33d73e882
Petra B <petrabrinck@icloud.com>
2020-03-05 17:17:53 +0100

    testing the cloned project and branch

--- frontend/src/components/MyDay.js
+++ frontend/src/components/MyDay.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './myday.css'
 import { Link } from 'react-router-dom'
 
+// TESTING THE CLONED PROJECT AND THE BRANCH 
 
 const apiKey = `563492ad6f917000010000016674d16c530e444482c459f1837b2a47`
 const url = "https://api.pexels.com/v1/search?query=example+beach&per_page=25&page=1"
@@ -17,7 +18,7 @@ export const MyDay = () => {
                 console.log(json)
             })
     }, [])
-    
+
     // function goBack() {
     //     window.history.back();
     //   }
