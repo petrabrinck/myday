88b625217a52fb704d441c77998de77893744781
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-03-09 11:25:03 +0100

    frontend and backend connected

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,40 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/myday'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+// const Reflection = mongoose.model('Reflection', {
+//   message: {
+//     type: String,
+//     required: true,
+//     minlength: 5,
+//     maxlength: 140
+//   },
+
+//   checkbox: { 
+//     type: String,
+//     // required: true,
+//     enum: ['Happy', 'Worried', 'Strong', 'Sad'],
+//     placeholder: "Text",
+//     forceField: "checkbox"
+// },
+
+  // hearts: {
+  //   type: Number,
+  //   default: 0
+  // },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now
+//   }
+// })
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -23,7 +48,38 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+// //Theresa added down below from 51-55
+// app.get('/', async (req, res) => {
+//   const reflections = await Reflection.find().sort({createdAt: 'desc'}).limit(140).exec()
+//   res.json(reflections)
+// })
+
+// app.post('/', async (req, res) => {
+//   //Retrieve the information sent by the client to our API endpoint
+//   const {message} = req.body
+
+//   //Use our mongoose model to create the database entry
+//   const reflection = new Reflection({message})
+
+//   try {
+//     //Success
+//     const savedReflection = await reflection.save()
+//     res.status(201).json(savedReflection)
+//   }catch (err) {
+//     res.status(400).json({message: 'Could not save your reflection', error: err.errors}) 
+//   }
+// })
+
+// app.post('/reflection', async (req, res) => {
+//     const reflection = new Reflection({ message }) // removed checkbox allt här som ska sparas
+//     const savedReflection = await reflection.save()
+//     res.status(201).json(savedReflection)
+// })
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MyDay } from './components/MyDay'
 import { Header } from './components/Header'
@@ -6,7 +6,36 @@ import { MyDay2 } from './components/MyDay2'
 
 // import './App.css';
 
+// const url = "https://project-happy-thoughts-api.herokuapp.com/"
+const url = "https://localhost:8080/reflection"
+
+//Theresa added const checkboxes down below
+//If time ask Jennie about down below sign
 export const App = () => {
+  const [reflections, setReflections] = useState([])
+  const [checkboxes, setCheckboxes] = useState([])
+  const [postedReflection, setPostedReflection] = useState("")
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => setReflections(json))
+  }, [postedReflection])
+
+  //Theresa changed from reflection to message down below
+  const handleFormSubmit = message => {
+    fetch(url, {
+      method: "POST",
+      //Theresa changed from reflection to message down below and took away checkbox
+      body: JSON.stringify({ message }),
+      headers: { "Content-Type": "application/json" }
+    })
+    //Theresa changed from reflection to message down below
+      .then(() => setPostedReflection(message))
+      .catch(err => console.log("error:", err))
+  }
+//Theresa hided down below export as we have it once already at the top
+// export const App = () => {
   return (
     <BrowserRouter>
       <Switch>
@@ -16,7 +45,8 @@ export const App = () => {
         </Route>
         <Route path="/photos/:myday2/">
           {/* <Route path="/photos/:photoId"> */}
-          <MyDay2 />
+          {/* <MyDay2 /> */}
+          <MyDay2 onFormSubmit={handleFormSubmit} />
         </Route>
       </Switch>
     </BrowserRouter>
