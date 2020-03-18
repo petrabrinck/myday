2ac832d4740201dbc3373f248611afcc55014096
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-03-18 15:14:02 +0100

    heroku update

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 // const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/myday'
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/myday-reflection'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/myday'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
