4f6f798f6a47896a8b6b839f03214bec0ee61819
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-04-08 22:50:06 +0200

    social media iconns installed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import Linkedin from 'assets/linkedin.png'
+import Instagram from 'assets/instagram.png'
+import Facebook from 'assets/facebook.png'
 import { NewDayButton } from './NewDayButton'
 import { NavigationBar } from './NavigationBar'
 
@@ -7,6 +10,32 @@ export const Header = () => {
   return (
     <section className='headerSection'>
       <NavigationBar />
+      <div class="header-icons">
+          <div class="hIconCircle">
+            <a href="https://www.linkedin.com/in/theresa-ulwahn-b719259/" target="_blank">
+              <img
+                src={Linkedin} target="_blank"
+                class="hIconImg"
+                alt="LinkedInLogo"
+            /></a>
+          </div>
+          <div class="hIconCircle">
+            <a href="https://www.facebook.com/theresa.ulwahn?ref=bookmarks" target="_blank">
+              <img
+                src={Facebook} target="_blank"
+                class="hIconImg"
+                alt="FacebookLogo"
+            /></a>
+          </div>
+          <div class="hIconCircle">
+            <a href="https://www.instagram.com/fruulw/" target="_blank">
+              <img
+                src={Instagram} target="_blank"
+                class="hIconImg"
+                alt="InstagramLogo"
+            /></a>
+          </div>
+        </div>
       <div className="headerInfo">
         <h1>My Day </h1> 
           <div className="welcomeText">  
