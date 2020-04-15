07cdc9e4e515252e9d774368bdf4823d8133205f
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-04-15 11:33:36 +0200

    added value reset form to see if field is cleared

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -76,7 +76,7 @@ export const Login = () => {
               required
             ></input>
             {errorMsg && <div className="error-message"> {errorMsg} </div>}
-            <button type="submit">Login</button>
+            <button type="submit" value="Reset form">Login</button>
           </div>
 
           <div className="submitButton">   
