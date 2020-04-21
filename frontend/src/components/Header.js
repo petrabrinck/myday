5d44f13a93478d56e1df647cf58d12b30cc8a162
Petra B <petrabrinck@icloud.com>
2020-04-21 13:45:03 +0200

    adjustments in navbar. Redirected newday link and erased reflections

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,43 +11,43 @@ export const Header = () => {
     <section className='headerSection'>
       <NavigationBar />
       <div className="header-icons">
-          <div className="hIconCircle">
-            <a href="https://www.linkedin.com/in/theresa-ulwahn-b719259/" target="_blank">
-              <img
-                src={Linkedin} target="_blank"
-                className="hIconImg"
-                alt="LinkedInLogo"
+        <div className="hIconCircle">
+          <a href="https://www.linkedin.com/in/petra-celie-brinck-5aa06221/" target="_blank">
+            <img
+              src={Linkedin} target="_blank"
+              className="hIconImg"
+              alt="LinkedInLogo"
             /></a>
-          </div>
-          <div className="hIconCircle">
-            <a href="https://www.facebook.com/theresa.ulwahn?ref=bookmarks" target="_blank">
-              <img
-                src={Facebook} target="_blank"
-                className="hIconImg"
-                alt="FacebookLogo"
-            /></a>
-          </div>
-          <div className="hIconCircle">
-            <a href="https://www.instagram.com/fruulw/" target="_blank">
-              <img
-                src={Instagram} target="_blank"
-                className="hIconImg"
-                alt="InstagramLogo"
-            /></a>
-          </div>
         </div>
+        <div className="hIconCircle">
+          <a href="https://www.facebook.com/petra.brinck" target="_blank">
+            <img
+              src={Facebook} target="_blank"
+              className="hIconImg"
+              alt="FacebookLogo"
+            /></a>
+        </div>
+        <div className="hIconCircle">
+          <a href="https://www.instagram.com/petrabrinck/" target="_blank">
+            <img
+              src={Instagram} target="_blank"
+              className="hIconImg"
+              alt="InstagramLogo"
+            /></a>
+        </div>
+      </div>
       <div className="headerInfo">
-        <h1>My Day </h1> 
-          <div className="welcomeText">  
-            <h4>Most of us have some kind of social media app where we spend time, but there is seldom any space for inner reflections. 
-              We believe that it's essential for our wellbeing to reflect on a daily basis. </h4> 
-            {/* <h4 className="extraText">Most of us have some kind of social media app where we spend time, but there is seldom any space for inner reflections. 
+        <h1>My Day </h1>
+        <div className="welcomeText">
+          <h4>Most of us have some kind of social media app where we spend time, but there is seldom any space for inner reflections.
+              We believe that it's essential for our wellbeing to reflect on a daily basis. </h4>
+          {/* <h4 className="extraText">Most of us have some kind of social media app where we spend time, but there is seldom any space for inner reflections. 
               We believe that it's essential for our wellbeing to reflect on a daily basis. </h4>  */}
-            <h4> This is a journal where you can write your thoughts and look back at your previous posts. </h4>
-          </div>
+          <h4> This is a journal where you can write your thoughts and look back at your previous posts. </h4>
+        </div>
       </div>
       <div className="newDayButtonDiv">
-          <NewDayButton />
+        <NewDayButton />
       </div>
     </section>
   )
--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar, { ThemeProps } from '@ovie/react-navbar'
 import './navigationbar.css'
 
@@ -9,12 +10,12 @@ const navTheme: ThemeProps = {
 
 // const navBrand = <a className="brand" href="#null">Base Corp.</a>
 
-const navBrand = <a className="brand" href="#null"><p>My Day</p></a> 
+const navBrand = <a className="brand" href="#null"><p>My Day</p></a>
 
 const leftLinks = (
   <>
-    {/* <a href="/newReflection/">New Day</a>
-    <a href="#null">Your Reflections</a> */}
+    <a href="/newReflection/">New Day</a>
+    {/* <a href="#null">Your Reflections</a> */}
   </>
 )
 
@@ -22,32 +23,38 @@ const handleLogOut = () => {
   console.log("Hej!")
   window.localStorage.removeItem("accessToken")
   window.localStorage.removeItem("userId")
-
 }
 
 const rightLinks = (
   <>
+    <Link to={`/newReflection/`}>
+      <a href="/newReflection/">New Day</a>
+    </Link>
     <a onClick={handleLogOut} href="/">Log Out</a>
-    {/* <a href="#null">Sign Up</a> */}
-    <a href="/newReflection/">New Day</a>
-    <a href="#null">Your Reflections</a>
   </>
 )
+// const rightLinks = (
+//   <>
+//     <a onClick={handleLogOut} href="/">Log Out</a>
+{/* <a href="#null">Sign Up</a> */ }
+{/* <a href="/newReflection/">New Day</a> */ }
+{/* <a href="#null">Your Reflections</a> */ }
+{/* </>
+) */}
 
 
 
 export const NavigationBar = () => {
-    return (
-      <div className="navMenu">
-        <Navbar
-          brand={navBrand}
-          theme={navTheme}
-          leftLinks={leftLinks}
-          rightLinks={rightLinks}
-          shouldAnimate={true}
-        />
-      </div>
-    
-    )
-  }
-  
+  return (
+    <div className="navMenu">
+      <Navbar
+        brand={navBrand}
+        theme={navTheme}
+        leftLinks={leftLinks}
+        rightLinks={rightLinks}
+        shouldAnimate={true}
+      />
+    </div>
+
+  )
+}
