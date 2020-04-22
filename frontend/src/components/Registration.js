b587f574b240018409e3186b547fa5a0bbd57fec
Petra B <petrabrinck@icloud.com>
2020-04-22 14:12:13 +0200

    changing sign up form css

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import './registration.css' 
+import './registration.css'
 
 // const url = 'https://myday-reflection.herokuapp.com/register'
 const URL = 'https://authorisation-app.herokuapp.com/register'
@@ -9,38 +9,40 @@ const URL = 'https://authorisation-app.herokuapp.com/register'
 
 
 export const Registration = () => {
- const[name, setName] = useState('')
- const[email, setEmail] = useState('')
- const[password, setPassword] = useState('')
- const[repeat, setRepeat] = useState('')
- const [errorMsg, setErrorMsg] = useState(null)
- const [successMsg, setSuccessMsg] = useState(null)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  //  const[repeat, setRepeat] = useState('')
+  const [errorMsg, setErrorMsg] = useState(null)
+  const [successMsg, setSuccessMsg] = useState(null)
 
 
   // To sign up a user.
   const handleFormSubmit = event => {
     event.preventDefault()
-    
+
     fetch(URL, {
       method: 'POST',
-      body: JSON.stringify({ name, email, password, repeat }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' }
     })
       .then(res => {
         if (res.ok) {
-          setSuccessMsg("User created - we'll send you back to log-in now!") 
-          setErrorMsg(false) 
+          setSuccessMsg("User created - we'll send you back to log-in now!")
+          setErrorMsg(false)
           return res.json()
         }
         else {
-          setErrorMsg("could not create user") 
-          setSuccessMsg(false) 
+          setErrorMsg("could not create user")
+          setSuccessMsg(false)
           return res.text().then(json => { throw new Error(json) })
         }
       })
-      .then(user => {console.log("created user:", user)
-    setTimeout( () => { window.location.href="/"; }, 3000);})
-      .catch(err => { console.error(err) }) 
+      .then(user => {
+        console.log("created user:", user)
+        setTimeout(() => { window.location.href = "/"; }, 3000);
+      })
+      .catch(err => { console.error(err) })
 
   }
 
@@ -54,32 +56,32 @@ export const Registration = () => {
         </a>
         <h1><strong>Sign up</strong></h1>
         <h2>Not a member? Fill in this form and you're set!</h2>
-        <div className="infoContainer">  
-          <label>Name:</label> 
+        <div className="infoContainer">
+          <label>Name:</label>
           <input value={name} placeholder="Enter Name" type="text" name="name" onChange={event => setName(event.target.value)} required></input>
-        
+
           <label >Email:</label>
-          <input value={email} placeholder="Enter Email"type="email" name="email" onChange={event => setEmail(event.target.value)} required></input>
-          
+          <input value={email} placeholder="Enter Email" type="email" name="email" onChange={event => setEmail(event.target.value)} required></input>
+
           <label>Password:</label>
-          <input value={password} placeholder="Enter Password" type="password"  name="password" onChange={event => setPassword(event.target.value)} required> 
+          <input value={password} placeholder="Enter Password" type="password" name="password" onChange={event => setPassword(event.target.value)} required>
           </input>
-    
-          <label>Repeat Password:</label>
-          <input value={repeat} placeholder="Repeat Password" type="password"  name="repeat" onChange={event => setRepeat(event.target.value)} required>
-          </input>
+
+          {/* <label>Repeat Password:</label>
+          <input value={repeat} placeholder="Repeat Password" type="password" name="repeat" onChange={event => setRepeat(event.target.value)} required>
+          </input> */}
           {/* success and error messages */}
           {errorMsg && <div className="error-message"> {errorMsg} </div>}
           {successMsg && <div className="success-message"> {successMsg} </div>}
 
           <button onClick={handleFormSubmit} type="submit" >Submit</button>
-        
-          <div className="toSignInButton">   
-            <Link to={`/`}> 
+
+          <div className="toSignInButton">
+            <Link to={`/`}>
               <button
-                >Already a member? Sign in
+              >Already a member? Sign in
               </button>
-            </Link> 
+            </Link>
           </div>
         </div>
       </form>
